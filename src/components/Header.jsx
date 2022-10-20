f6c4274872d942cbb80cc0ebfe83b8c31365ee85
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-20 13:14:36 +0300

    AddTaskForm finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddTaskForm from './AddTaskForm'
+
+const Header = () => {
+  return (
+    <div>
+      <AddTaskForm/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/ShowTasks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowTasks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShowTasks
