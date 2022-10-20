f8fd916ff85a7c648fc781f98f3184b7a9791d99
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-20 15:19:53 +0300

    home component finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import Header from "../components/Header"
-import ShowTasks from "../components/ShowTasks"
+import React from 'react';
+import Header from '../components/Header';
+import ShowTasks from '../components/ShowTasks';
+import { useState, useEffect } from 'react';
+// import data from '../helper/starterData';
 
 const Home = () => {
-  return (
-    <div>
-      <Header/>
-      <ShowTasks/>
-    </div>
-  )
-}
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  );
 
-export default Home
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  console.log(tasks);
+  return (
+    <div className="container">
+      <Header tasks={tasks} setTasks={setTasks} />
+      {tasks.length > 0 ? (
+        <ShowTasks tasks={tasks} setTasks={setTasks} />
+      ) : (
+        <p className="text-center">NO TASK TO SHOW</p>
+      )}
+    </div>
+  );
+};
+
+export default Home;
+
